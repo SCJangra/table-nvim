4adba50b616ebf499e4a67ae0fbee180e58e575b
Sachin Charakhwal <sachincharakhwal@gmail.com>
2024-08-04 13:38:29 +0530

    change update event to `InsertLeavePre`

--- lua/table-nvim/init.lua
+++ lua/table-nvim/init.lua
@@ -6,7 +6,7 @@ local api = vim.api
 local ts = vim.treesitter
 
 local group_id = api.nvim_create_augroup('table-nvim', { clear = true })
-api.nvim_create_autocmd({ 'InsertLeave' }, {
+api.nvim_create_autocmd({ 'InsertLeavePre' }, {
   group = group_id,
   pattern = '*.md',
   callback = function()
