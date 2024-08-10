8bd94c126d02546d93bc23bff499a8b72cfa9aef
Sachin Charakhwal <sachincharakhwal@gmail.com>
2024-08-10 17:18:22 +0530

    utils: add more functions

--- lua/table-nvim/utils.lua
+++ lua/table-nvim/utils.lua
@@ -1,4 +1,6 @@
 local tbl_node = 'pipe_table'
+local tbl_cell = 'pipe_table_cell'
+local tbl_delimiter_cell = 'pipe_table_delimiter_cell'
 local tbl_node_len = #tbl_node
 
 ---Returns `true` if the node is the root of a markdown table and `false` otherwise.
@@ -26,8 +28,34 @@ local get_tbl_root = function(node)
   end
 end
 
+---Check if a positon is withing some range.
+---@param row number Row position to check.
+---@param col number Column position to check.
+---@param row_start number Starting row of the range.
+---@param col_start number Starting column of the range.
+---@param row_end number Ending row of the range.
+---@param col_end number Ending column of the range.
+local is_in_range = function(row, col, row_start, col_start, row_end, col_end)
+  if (row == row_start and row == row_end and col >= col_start and col <= col_end) or
+      (row == row_start and row < row_end and col >= col_start) or
+      (row > row_start and row == row_end and col <= col_end) or
+      (row > row_start and row < row_end) then
+    return true
+  else
+    return false
+  end
+end
+
+---Returns `true` if the provided node is a table cell and `false` otherwise.
+local is_tbl_cell = function(node)
+  local type = node:type()
+  return type == tbl_cell or type == tbl_delimiter_cell
+end
+
 return {
   get_tbl_root = get_tbl_root,
   is_tbl_root = is_tbl_root,
   is_tbl_node = is_tbl_node,
+  is_in_range = is_in_range,
+  is_tbl_cell = is_tbl_cell,
 }
