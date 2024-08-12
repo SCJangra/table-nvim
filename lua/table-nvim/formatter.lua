98e522d9f88faa9b6594283023ad0001d8d605b2
Sachin Charakhwal <sachincharakhwal@gmail.com>
2024-08-12 22:57:33 +0530

    formatter: collect information about the column types

--- lua/table-nvim/formatter.lua
+++ lua/table-nvim/formatter.lua
@@ -7,12 +7,22 @@ local conf = require('table-nvim.config')
 -- The second row should always be the delimiter row
 local delimiter_row = 2
 
+---@enum ColumnType
+local ColumnType = {
+  Delimiter = 0,
+  Regular = 1,
+}
+
+---@class (exact) ColumnInfo Information about a column in the table, including the delimiter (`|`) columns.
+---@field type ColumnType Type of the column.
+---@field width number Text width of the column.
+
 ---@class (exact) Formatter Provides functionality to format markdown tables.
 ---@field start number Index of the first row in the table.
 ---@field end_ number Index of the last row in the table.
 ---@field indent number Indentation of the table.
 ---@field rows string[][] Rows in the table, each row is an array of strings.
----@field widths number[] Widths of each column of the table.
+---@field cols ColumnInfo[] Information about all columns in the table.
 ---@field cursor_col number The current column position of the cursor.
 local Formatter = {}
 
@@ -29,7 +39,7 @@ function Formatter:new(root)
   local start = root:start();
   local end_ = root:end_();
   local indent
-  local widths = {}
+  local cols = {}
   local rows = {}
   local cursor_col_index = 1
 
@@ -42,13 +52,21 @@ function Formatter:new(root)
     local c = 0
     for col in row:iter_children() do
       c = c + 1
+      cols[c] = cols[c] or {}
 
       local text = ts.get_node_text(col, 0):match('^%s*(.-)%s*$')
       local width = #text
+      local type = col:type()
 
       if ts.is_in_node_range(col, cursor_row, cursor_col) then cursor_col_index = c end
 
-      if config.padd_column_separators and text == '|' then
+      if type == '|' then
+        cols[c].type = ColumnType.Delimiter
+      else
+        cols[c].type = ColumnType.Regular
+      end
+
+      if config.padd_column_separators and type == '|' then
         if c == 1 then
           text = '| '
         elseif c == c_count then
@@ -62,11 +80,12 @@ function Formatter:new(root)
 
       if r == 1 then
         if c == 1 then _, indent = col:start() end
-        widths[c] = width
+        cols[c].width = width
+        print('First row and', r, 'column', c, 'with width', width)
       elseif r == delimiter_row then
         -- Do nothing
       else
-        widths[c] = math.max(width, widths[c] or 0)
+        cols[c].width = math.max(width, cols[c].width or 0)
       end
 
       rows[r][c] = text
@@ -79,7 +98,7 @@ function Formatter:new(root)
     start = start,
     end_ = end_,
     indent = indent,
-    widths = widths,
+    cols = cols,
     rows = rows,
     cursor_col = cursor_col_index,
   }
@@ -94,6 +113,8 @@ end
 function Formatter:render()
   local lines = {}
 
+  vim.print(self)
+
   for r, row in ipairs(self.rows) do
     local line = {}
 
@@ -103,13 +124,13 @@ function Formatter:render()
       end
 
       local width = #col
-      local max_width = self.widths[c]
+      local max_width = self.cols[c].width
 
       local padding = r == delimiter_row and '-' or ' '
 
       if width < max_width then
         table.insert(line, col)
-        if r == delimiter_row or c < #self.widths then
+        if r == delimiter_row or c < #self.cols then
           table.insert(line, string.rep(padding, max_width - width))
         end
       elseif width > max_width then
